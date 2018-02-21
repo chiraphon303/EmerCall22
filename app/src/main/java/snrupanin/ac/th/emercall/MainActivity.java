32a32f6cf9e89cf3bbee512242e94f2f4d25cfa8
Chiraphon <Chiraphon.ba59@snru.ac.th>
2018-02-21 11:24:31 +0700

    Darwer manu saccas

--- app/src/main/java/snrupanin/ac/th/emercall/MainActivity.java
+++ app/src/main/java/snrupanin/ac/th/emercall/MainActivity.java
@@ -1,19 +1,43 @@
 package snrupanin.ac.th.emercall;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import snrupanin.ac.th.emercall.fragment.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Explicit
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Add Fragment
+//        Create Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = findViewById(R.id.drawerLayoutMain);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+         MainActivity.this,
+                drawerLayout,
+                R.string.open,
+                R.string.close
+        );
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
+//        Add Fragment
         if (savedInstanceState == null) {
 
             getSupportFragmentManager().beginTransaction()
@@ -22,4 +46,30 @@ public class MainActivity extends AppCompatActivity {
 
     }   // Main Method
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return  true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+
+    }
 }   // Main Class
